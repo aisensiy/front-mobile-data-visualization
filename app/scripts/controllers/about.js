497314d3658b913ba10c01ac392ba1c1d86fe3de
aisensiy <aisensiy@gmail.com>
2015-03-02 16:29:46 +0800

    Init commit: yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontMobileDataVisualizationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontMobileDataVisualizationApp
+ */
+angular.module('frontMobileDataVisualizationApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontMobileDataVisualizationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontMobileDataVisualizationApp
+ */
+angular.module('frontMobileDataVisualizationApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
