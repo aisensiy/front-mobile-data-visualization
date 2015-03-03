d86a8e3599634aed5d0d0426f1c95618385a6474
aisensiy <aisensiy@gmail.com>
2015-03-03 11:32:29 +0800

    add baidumap directive to show location list

--- app/scripts/controllers/baidumap.js
+++ app/scripts/controllers/baidumap.js
@@ -8,10 +8,13 @@
  * Controller of the frontMobileDataVisualizationApp
  */
 angular.module('frontMobileDataVisualizationApp')
-  .controller('BaidumapCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('BaidumapCtrl', ['$scope', function ($scope) {
+      var locations = [
+        ['20141201081101', '40.46181 115.96892'],
+        ['20141201081105', '40.46181 115.96892'],
+        ['20141201121203', '40.46181 115.96392'],
+        ['20141201131811', '40.46181 115.96892'],
+        ['20141201181100', '40.46181 115.96892']
+      ];
+      $scope.locations = locations;
+  }]);
