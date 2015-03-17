3934acdfaa46974e8d940d474af01ddcfb93f01b
aisensiy <aisensiy@gmail.com>
2015-03-17 12:55:09 +0800

    change log by day

--- app/scripts/controllers/app-logs.js
+++ app/scripts/controllers/app-logs.js
@@ -9,7 +9,13 @@
  */
 angular.module('frontMobileDataVisualizationApp')
   .controller('AppLogsCtrl', ['api', '$scope', '$routeParams', function (api, $scope, $routeParams) {
-    api.app_log_by_uid_day($routeParams.uid, '01').success(function(data) {
-      $scope.logs = data;
+    api.location_daycount_by_uid($routeParams.uid).success(function(data) {
+      $scope.dates = data;
     });
+    $scope.getlog = function(day) {
+      $scope.cur_day = day;
+      api.app_log_by_uid_day($routeParams.uid, day.slice(6, 8)).success(function(data) {
+        $scope.logs = data;
+      });
+    };
   }]);
