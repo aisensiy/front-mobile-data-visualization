4da5622b03007d3fcc2a1a1cdc61caee558ffae6
aisensiy <aisensiy@gmail.com>
2015-03-17 10:43:15 +0800

    add route userlist

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,10 @@ angular
         templateUrl: 'views/map.html',
         controller: 'MapCtrl'
       })
+      .when('/userlist', {
+        templateUrl: 'views/userlist.html',
+        controller: 'UserlistCtrl'
+      })
       .otherwise({
         redirectTo: '/net'
       });
